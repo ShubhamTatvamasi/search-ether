f255b4b7ecfd1a3ce9dde656255e2cb49b6b7320
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-06-22 14:36:23 +0530

    new file:   .gitignore 	modified:   app.js 	new file:   package-lock.json

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const EthereumWallet = require('ethereumjs-wallet')
-const EthereumUtil = require('ethereumjs-util')
-const Web3 = require('web3')
-const web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io"))
 const fs = require('fs')
+const Web3 = require('web3')
+const EthereumUtil = require('ethereumjs-util')
+const EthereumWallet = require('ethereumjs-wallet')
+const web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io"))
 
-setInterval( () => {
+setInterval(() => {
 
-  var wallet = EthereumWallet.generate()
-  var walletPrivateKey = wallet.privKey
+	const wallet = EthereumWallet.generate()
+  const walletPrivateKey = wallet.privKey
 
-  var publicKeyBuff = EthereumUtil.privateToAddress(walletPrivateKey)
-  var privateKey = EthereumUtil.bufferToHex(walletPrivateKey)
-  var publicKey = EthereumUtil.bufferToHex(publicKeyBuff)
-  var publicKeyCheckSum = EthereumUtil.toChecksumAddress(publicKey)
+  const publicKeyBuff = EthereumUtil.privateToAddress(walletPrivateKey)
+  const privateKey = EthereumUtil.bufferToHex(walletPrivateKey)
+  const publicKey = EthereumUtil.bufferToHex(publicKeyBuff)
+  const publicKeyCheckSum = EthereumUtil.toChecksumAddress(publicKey)
 
 	web3.eth.getBalance(publicKeyCheckSum, (error, result) => {
 
 		if (result > 0) {
-			var answer = publicKeyCheckSum + '\n' + privateKey + '\n' + result + '\n\n'
+			const answer = publicKeyCheckSum + '\n' + privateKey + '\n' + result + '\n\n'
 			fs.appendFile('result.txt', answer, () => {})
 			console.log(answer)
 		} else {
